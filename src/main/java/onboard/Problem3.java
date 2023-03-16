e9fbea347d2cfe61db637376b606a6862ccff246
Soyeon Cha <89539031+Soyeon-Cha@users.noreply.github.com>
2023-03-16 16:04:08 +0900

    ✨ feat: add Problem3 answer

--- src/main/java/onboard/Problem3.java
+++ src/main/java/onboard/Problem3.java
@@ -6,6 +6,23 @@ import java.util.List;
 public class Problem3 {
     public static List<Integer> solution(List<Integer> numbers){
         List<Integer> answer = new ArrayList<>();
+
+        int sum=0;
+        int a=0;
+        int arr[] = new int[5];
+
+        for(int i=0;i<5;i++) {
+            a = numbers.get(i);
+            arr[i] = a;
+            sum += a;
+        }
+
+        int avg = sum/5;
+
+        Arrays.sort(arr);
+        answer.set(0, avg);
+        answer.set(2, arr[2]);
+
         return answer;
     }
 }
