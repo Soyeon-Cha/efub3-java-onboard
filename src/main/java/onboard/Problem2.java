97e454e7e02bf344eceee6f425579a11932fa582
Soyeon Cha <89539031+Soyeon-Cha@users.noreply.github.com>
2023-03-16 22:30:34 +0900

    🔧 fix: update Problem2 answer

--- src/main/java/onboard/Problem2.java
+++ src/main/java/onboard/Problem2.java
@@ -2,20 +2,25 @@ package onboard;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Collections;
 
-public class Problem2 {
-    public static Integer[] solution(Integer[][] flipCards){
-        Integer[] answer = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
 
-        for(int i=0; i<10; i++) {
-            int a = flipCards[i][0];
-            int b = flipCards[i][1];
-            for (int j=(a-1); j<((a+b-2)/2); j++){
-                int temp = answer[j];
-                answer[j] = answer[(a+b-2-j)];
-                answer[(a+b-2-j)] = temp;
-            }
+
+public class Problem3 {
+    public static List<Integer> solution(List<Integer> numbers){
+        List<Integer> answer = new ArrayList<>();
+
+        int sum=0;
+
+        for(int i=0;i<5;i++) {
+            sum += numbers.get(i);
         }
+        int avg = sum/5;
+
+        Collections.sort(numbers);
+        answer.set(0, avg);
+        answer.set(1, numbers.get(2));
+
         return answer;
     }
 }
