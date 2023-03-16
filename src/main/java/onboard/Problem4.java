1e248ac13f31ed6ba8874933a37e83d0f7202fb3
Soyeon Cha <89539031+Soyeon-Cha@users.noreply.github.com>
2023-03-16 23:12:29 +0900

    Update Problem4.java

--- src/main/java/onboard/Problem4.java
+++ src/main/java/onboard/Problem4.java
@@ -1,5 +1,8 @@
 package onboard;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 /**
  * 리턴값 answer의 경우, 변수 초기값은 변경 가능합니다.
  * 다만 answer로 return하는 것은 변경하지 않도록 주의해주세요.
@@ -7,29 +10,14 @@ package onboard;
 public class Problem4 {
     public static Integer solution(Integer number){
         Integer answer = 0;
-        int a[] = new int[10];
+        char[] a = Integer.toString(number).toCharArray();
 
-        a[0] = number/1000000000;
-        a[1] = (number%1000000000)/100000000;
-        a[2] = (number%100000000)/10000000;
-        a[3] = (number%10000000)/1000000;
-        a[4] = (number%1000000)/100000;
-        a[5] = (number%100000)/10000;
-        a[6] = (number%10000)/1000;
-        a[7] = (number%1000)/100;
-        a[8] = (number%100)/10;
-        a[9] = number%10;
+        Arrays.sort(a);
 
-        for(int i=0; i<10; i++){
-            for(int j=i+1; j<10; j++){
-                if(a[i]<a[j]){
-                    int c = a[j];
-                    a[j] = a[i];
-                    a[i] = c;
-                }
-            }
+        for(int i=0; i<a.length; i++){
+            answer = 10*answer + a[(a.length-1-i)]-'0';
         }
-        
+
         return answer;
     }
 }
