88d20e4cd17d282232166243cb0c88578f9065b5
Soyeon Cha <89539031+Soyeon-Cha@users.noreply.github.com>
2023-03-16 13:02:51 +0900

    ✨ feat: add an answer for Problem1

--- src/main/java/onboard/Problem1.java
+++ src/main/java/onboard/Problem1.java
@@ -5,6 +5,18 @@ import java.util.List;
 
 public class Problem1 {
     public static List<Integer> solution(Integer hour, Integer minute){
+        if (minute>=45) {
+            minute -= 45;
+        }
+        else { // minute<45
+            if (hour>=1){
+                hour--;
+            }
+            else {
+                hour=23;
+            }
+            minute=minute+15;
+        }
         List<Integer> answer = new ArrayList<>();
         return answer;
     }
