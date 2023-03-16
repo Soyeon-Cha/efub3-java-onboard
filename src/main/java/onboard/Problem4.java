50287c171bcd1209c1837a6bd56c0a5d1bae2f0d
Soyeon Cha <89539031+Soyeon-Cha@users.noreply.github.com>
2023-03-16 22:57:45 +0900

    Update Problem4.java

--- src/main/java/onboard/Problem4.java
+++ src/main/java/onboard/Problem4.java
@@ -7,6 +7,29 @@ package onboard;
 public class Problem4 {
     public static Integer solution(Integer number){
         Integer answer = 0;
+        int a[] = new int[10];
+
+        a[0] = number/1000000000;
+        a[1] = (number%1000000000)/100000000;
+        a[2] = (number%100000000)/10000000;
+        a[3] = (number%10000000)/1000000;
+        a[4] = (number%1000000)/100000;
+        a[5] = (number%100000)/10000;
+        a[6] = (number%10000)/1000;
+        a[7] = (number%1000)/100;
+        a[8] = (number%100)/10;
+        a[9] = number%10;
+
+        for(int i=0; i<10; i++){
+            for(int j=i+1; j<10; j++){
+                if(a[i]<a[j]){
+                    int c = a[j];
+                    a[j] = a[i];
+                    a[i] = c;
+                }
+            }
+        }
+        
         return answer;
     }
 }
