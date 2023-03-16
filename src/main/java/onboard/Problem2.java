af08e3845b95e682f02f84f6c2ea0ed2a9cb51fb
Soyeon Cha <89539031+Soyeon-Cha@users.noreply.github.com>
2023-03-16 15:09:27 +0900

    🔧 fix: update Problem2 answer

--- src/main/java/onboard/Problem2.java
+++ src/main/java/onboard/Problem2.java
@@ -6,17 +6,18 @@ import java.util.List;
 public class Problem2 {
     public static Integer[] solution(Integer[][] flipCards){
         Integer[] answer = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
-        Integer[] middle = answer; // middle이 한 줄 시행 결과. answer는 시행 중의 저장소.
+        Integer[] middle = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20}; // middle이 한 줄 시행 결과. answer는 시행 중의 저장소.
 
         for(int i=0; i<10; i++) {
             int a = flipCards[i][0];
             int b = flipCards[i][1];
             for (int j=a; j<(b+1); j++){
-                middle[j] = answer[(a+b-j)];
+                int c = answer[a+b-j];
+                middle[j] = answer[c];
             }
             answer=middle; // answer에 한 줄 시행 결과 저장
         }
-        
+
         return answer;
     }
 }
