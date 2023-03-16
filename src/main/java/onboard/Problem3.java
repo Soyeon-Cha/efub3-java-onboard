ca21cf433ce26b2f2385556c949b2e56c3bb8d84
Soyeon Cha <89539031+Soyeon-Cha@users.noreply.github.com>
2023-03-16 22:50:21 +0900

    Update Problem3.java

--- src/main/java/onboard/Problem3.java
+++ src/main/java/onboard/Problem3.java
@@ -3,6 +3,7 @@ package onboard;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Collections;
+import java.util.Arrays;
 
 
 
@@ -15,10 +16,11 @@ public class Problem3 {
         for(int i=0;i<5;i++) {
             sum += numbers.get(i);
         }
-        int avg = sum/5;
-        int mid = numbers.get(2);
-
+        int avg = sum/numbers.size();
+        
         Collections.sort(numbers);
+        int mid = numbers.get(2);
+        
         answer.add(avg);
         answer.add(mid)
 
